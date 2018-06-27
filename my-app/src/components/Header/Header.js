f0fbca4824aab845775f3f21c91fefe3fff4c659
Danielthweatt <dthweatt192@yahoo.com>
2018-06-27 11:30:47 -0500

    added header component

---
+++ my-app/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => (
+    <header className="header">
+        <h1 className="header-title">
+            Clicky Game
+        </h1>
+        <h1 className="score-board">
+            Score: <span id="score">0</span> | High Score: <span id="high-score">0</span>
+        </h1>
+    </header>
+);
+
+export default Header;
