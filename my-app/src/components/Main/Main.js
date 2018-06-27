6804fdf8f4f0fdab36eb152e497c3c85ef171c2c
Danielthweatt <dthweatt192@yahoo.com>
2018-06-27 12:06:54 -0500

    added main component

---
+++ my-app/src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => (
+    <main>
+        <h1 className="info-title">
+            Click on an image to earn points, but don't click on any more than once!
+        </h1>
+    </main>
+);
+
+export default Main;
