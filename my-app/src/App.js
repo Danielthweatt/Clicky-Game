3fb58d0bc6702e6ceb4f6d12647b05b35c92304c
Danielthweatt <dthweatt192@yahoo.com>
2018-06-27 15:18:05 -0500

    added wrapper and image components, image folder, characters.json, worked on app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+import Wrapper from './components/Wrapper';
+import Image from './components/Image';
 import Footer from './components/Footer';
+import characters from './characters.json';
 
 class App extends Component {
+  state = {
+    characters,
+    charactersClicked: [],
+    highScore: undefined
+  };
+
   render() {
     return (
       <div>
         <Header />
-        <Main />
+        <Main>
+          <Wrapper>
+          {this.state.characters.map((character, i) => (
+          <Image
+            key={i}
+            name={character.name}
+            image={character.image}
+          />
+        ))}
+          </Wrapper>
+        </Main>
         <Footer />
       </div>
     );
--- my-app/src/components/Main/Main.js
+++ my-app/src/components/Main/Main.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Main.css";
 
-const Main = () => (
+const Main = props => (
     <main>
         <h1 className="info-title">
-            Click on an image to earn points, but don't click on any more than once!
+            Click on an image to earn points, but do not click on any more than once!
         </h1>
+        {props.children}
     </main>
 );
 
